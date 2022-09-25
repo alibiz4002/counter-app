79efb98ff0d90ba5aeb2c82a9c6edf854f775088
alibiz4002 <aliwebdev001@gmail.com>
2022-09-26 01:33:50 +0800

    change title text

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'i am checking'),
+      home: const MyHomePage(title: 'Hello wolrd'),
     );
   }
 }
